48a6c903629fc7917c96dc05dbf7155cfda52be0
Denis <103854079+tkachukds@users.noreply.github.com>
2023-04-21 12:47:18 +0300

    Update jsonModul.js

--- jsonModul.js
+++ jsonModul.js
@@ -11,20 +11,47 @@ function readTextFile(file, callback) {
 }
 
 let dataJSON;
+
 function loadingJsonFile(){
 readTextFile("BaseData.json", function(text){
 dataJSON = JSON.parse(text);//закинули в переменную data данные из файла
 //console.log(dataJSON);//вывели данные в консоль
 });
 }
-loadingJsonFile();//загрузка файла JSON в dataJSON
+
 let DataKey=[];
 let DataBASEKey;
 let loaddata = [];
+
+
 function LOADarrKEY(txt){
-    l(''+txt)
+    loadingJsonFile();//загрузка файла JSON в dataJSON
+    setTimeout(() => loadGOOD(txt), 2000);
+}
+
+function loadGOOD(txt){
+    editTXTAREAholder('загрузка базы данных')
+    if (dataJSON === undefined){
+        l('не успевает загрузиться json, или с ним что-то не так') 
+        loadingJsonFile();//загрузка файла JSON в dataJSON
+        setTimeout(() => inputDATAinbasearr(txt), 4000);
+        if (dataJSON === undefined){
+            editTXTAREAholder('Что-то не так с базой данных...не могу ее загрузить')
+            return a('Сорь, но с Json что-то не так')}
+    } else {
+        editTXTAREAholder('Что найти? Я знаю - '+txt)
+        l('c json все ок. начинаю запуск импорта базы') 
+        setTimeout(() => inputDATAinbasearr(txt), 1000);
+    }
+}
+function inputDATAinbasearr(txt){
 loaddata = dataJSON[txt];
 baseArray = loaddata;
+}
+
+function editTXTAREAholder(txt){ 
+    let search = document.getElementById('search_engine')
+    search.placeholder = txt;
 }
 
 function loadADMINbasedata(){
